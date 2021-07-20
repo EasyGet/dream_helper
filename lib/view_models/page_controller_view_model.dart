599b1874a60f6bb02150ed373884f5b3e0fdadf2
= <>
2021-07-20 16:48:46 +0800

    add page for PageControllerViewModel

--- lib/view_models/page_controller_view_model.dart
+++ lib/view_models/page_controller_view_model.dart
@@ -12,17 +12,20 @@ import 'controller_view_model.dart';
 
 /// PageControllerViewModel
 class PageControllerViewModel extends ControllerViewModel<PageController> {
+  int _current;
 
   /// construct with [pageController]
-  PageControllerViewModel(PageController pageController) : super(pageController);
+  PageControllerViewModel(PageController pageController, {int current = 0})
+      : _current = current,
+        super(pageController);
 
   /// 获取当前page索引
-  int get page =>
-      this.scrollController.page != null ? scrollController.page!.round() : 0;
+  int get page => _current;
 
   /// [_pageController] jump to [page]
   /// and notify listener
   void changePage(int page) {
+    _current = page;
     scrollController.jumpToPage(page);
     notifyListeners();
   }
